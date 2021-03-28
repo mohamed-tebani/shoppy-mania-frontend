cede3c36135dea3e979f345a9c455bc43555f529
Mohamed TEBANI <mohamed.tebani95@gmail.com>
2021-03-29 00:05:30 +0100

    Init config v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    TranslateModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
